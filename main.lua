7c64a853877a4cd4f942fbf1d5301b49dc0312df
sop201542 <candy07110918@gmail.com>
2026-04-24 20:48:19 +0800

    Refactor MasterHub V3 window and button setup
    Removed unused configuration options and updated button names.

--- main.lua
+++ main.lua
@@ -1,27 +1,6 @@
--- [[ MasterHub V3 - Stable Edition ]]
+print("GitHub 檔案讀取成功！")
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
-
-local Window = OrionLib:MakeWindow({
-    Name = "MasterHub V3", 
-    HidePremium = true, 
-    SaveConfig = true, 
-    ConfigFolder = "MHV3"
-})
-
-local Tab = Window:MakeTab({
-    Name = "主要功能",
-    Icon = "rbxassetid://4483345998"
-})
-
-Tab:AddButton({
-    Name = "點擊測試",
-    Callback = function()
-        OrionLib:MakeNotification({
-            Name = "系統",
-            Content = "雲端加載成功！",
-            Time = 5
-        })
-    end
-})
-
+local Window = OrionLib:MakeWindow({Name = "MasterHub V3", HidePremium = true})
+local Tab = Window:MakeTab({Name = "功能"})
+Tab:AddButton({Name = "成功了！", Callback = function() print("Works!") end})
 OrionLib:Init()
